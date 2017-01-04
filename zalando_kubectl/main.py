acb3fe03f88e6e7959af42dedcf57e106e1f64d7
Henning Jacobs <henning@zalando.de>
2017-01-04 10:30:06 +0100

    update to kubectl 1.5.1

--- zalando_kubectl/main.py
+++ zalando_kubectl/main.py
@@ -12,7 +12,7 @@ from . import kube_config
 
 APP_NAME = 'zalando-kubectl'
 KUBECTL_URL_TEMPLATE = 'https://storage.googleapis.com/kubernetes-release/release/{version}/bin/{os}/{arch}/kubectl'
-KUBECTL_VERSION = 'v1.4.7'
+KUBECTL_VERSION = 'v1.5.1'
 
 
 def ensure_kubectl():
@@ -26,6 +26,7 @@ def ensure_kubectl():
         url = KUBECTL_URL_TEMPLATE.format(version=KUBECTL_VERSION, os=platform, arch=arch)
         with Action('Downloading {}..'.format(url)) as act:
             response = requests.get(url, stream=True)
+            response.raise_for_status()
             local_file = kubectl + '.download'
             with open(local_file, 'wb') as fd:
                 for i, chunk in enumerate(response.iter_content(chunk_size=4096)):
